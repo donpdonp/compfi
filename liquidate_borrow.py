0e810e69898e948ad6e20ea1079ace595da15363
Don <donp@donp.org>
2022-06-22 10:58:41 -0700

    ropsten gas guzzler

--- liquidate_borrow.py
+++ liquidate_borrow.py
@@ -4,9 +4,9 @@ from web3 import Web3
 with open(f'config.json') as file:
   config = json.load(file)
 
-print(config['provider'])
+print(f"{config['network']} {config['provider']}")
 web3 = Web3(Web3.HTTPProvider(config['provider']))
-print(f"connected: {web3.isConnected()}")
+# print(f"connected: {web3.isConnected()}")
 
 with open(f'compound-config/networks/{config["network"]}-abi.json') as file:
   compound_abi = json.load(file)
@@ -21,9 +21,22 @@ contract = web3.eth.contract(address=cETH_address, abi=ceth_abi)
 c_token_collateral = cETH_address
 go = contract.functions.liquidateBorrow(config['borrower'], c_token_collateral)
 try:
-  print(f"estimateGas {go.estimateGas()}")
-  txn_hash = go.transact({"from": config['me']})
-  print(txn_hash)
+  opts = {"from": config['me_pub'], 
+          "value": web3.toWei(0.1, 'ether'),
+          "gas": 100000,
+          "gasPrice": web3.toWei(5000, 'gwei'),
+        }
+  print(opts)
+  # txn_hash = go.transact(opts)
+
+  opts['nonce'] = web3.eth.get_transaction_count(config['me_pub'])
+  print(f"nonce {opts['nonce']}")
+  txn = go.buildTransaction(opts)
+  signed_txn = web3.eth.account.signTransaction(txn, private_key=config['me_priv'])
+  txn_hash = web3.eth.send_raw_transaction(signed_txn.rawTransaction)
+  # txn_hash = web3.toHex(web3.keccak(signed_txn.rawTransaction))
+  print(web3.toHex(txn_hash))
+  print("send. waiting for receipt.")
   web3.eth.wait_for_transaction_receipt(txn_hash)
 except Exception as e:
   print(f"error: {e}")
