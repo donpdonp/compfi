3c954557f06293c99d0c55c4eabf0d664ab9beed
Don <donp@donp.org>
2022-06-23 11:13:30 -0700

    extract argument_parser

--- compfi/__main__.py
+++ compfi/__main__.py
@@ -1,19 +1,8 @@
-import argparse
+import compfi.argument_parser
 from .cmds import approve, liquidate_borrow
 
 def main():
-    main_parser = argparse.ArgumentParser(description='compound.finance tool')
-    cmd_parser = main_parser.add_subparsers(help='commands')
-
-    lb_parser = cmd_parser.add_parser('liquidateBorrow', help='call liquidateBorrow')
-    lb_parser.add_argument("addr")
-    lb_parser.set_defaults(func=liquidate_borrow.go)
-
-    ap_parser = cmd_parser.add_parser('approve', help='call approve')
-    ap_parser.set_defaults(func=approve.go)
-    ap_parser.add_argument("addr")
-
-    args = main_parser.parse_args()
+    args = compfi.argument_parser.parse()
     if "func" in args:
         args.func(vars(args))
     else:
---
+++ compfi/cmds/liquidate_borrow.py
@@ -0,0 +1,2 @@
+def go(x):
+    print(f"I am liquidate_borrow{x}")
